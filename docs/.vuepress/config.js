e13c1d546e65398c1127bd5c46e2a605d8dd436a
jihyunhillpark <girl9505@naver.com>
2022-11-09 00:47:30 +0900

    (add) config setting.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default {
+  theme: defaultTheme({
+    lang: 'en-US',
+    title: 'jihyunee blog',
+    description: 'jihyun\'s dev&tech blog',
+    sidebar: 'auto',
+  }),
+}
