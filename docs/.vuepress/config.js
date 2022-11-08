10ee631f4ac0c8d3d4e711f1808179df8c69cdcd
jihyunhillpark <girl9505@naver.com>
2022-11-09 03:13:08 +0900

    (add) add locale config.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,14 @@
 import { defaultTheme } from '@vuepress/theme-default'
 
 export default {
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'jihyunee blog',
+      description: 'jihyun\'s dev&tech blog'
+    },
+  },
   theme: defaultTheme({
-    lang: 'en-US',
-    title: 'jihyunee blog',
-    description: 'jihyun\'s dev&tech blog',
     sidebar: 'auto',
   }),
 }
